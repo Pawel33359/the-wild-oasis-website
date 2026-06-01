04a43eefa3756ab1a738c264fe9dd88f97fd9085
Pawel33359 <pawecio33359@gmail.com>
2026-06-01 13:38:49 +0200

    Finish

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -32,6 +32,35 @@ export async function updateGuest(formData) {
   revalidatePath("/account/profile");
 }
 
+export async function createBooking(bookingData, formData) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const newBooking = {
+    ...bookingData,
+    guestId: session.user.guestId,
+    numGuests: +formData.get("numGuests"),
+    observations: formData.get("observations").slice(0, 1000) || "",
+    extrasPrice: 0,
+    totalPrice: bookingData.cabinPrice,
+    status: "unconfirmed",
+    isPaid: false,
+    hasBreakfast: false,
+  };
+
+  // Make sure the dates dont overlap with existing bookings for the same cabin
+
+  const { error } = await supabase.from("bookings").insert(newBooking);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Booking could not be created");
+  }
+
+  revalidatePath("/cabins/" + bookingData.cabinId);
+  redirect("/cabins/thankyou");
+}
+
 export async function deleteBooking(bookingId) {
   const session = await auth();
   if (!session) throw new Error("You must be logged in");
